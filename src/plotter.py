64169351b43054d55db0ce196b2c447ff93819aa
Celsa Diaz <celsadiaz@Celsas-Air.fritz.box>
2018-08-21 22:33:38 +0200

    add visualizations for article

--- src/plotter.py
+++ src/plotter.py
@@ -2,15 +2,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def plot_time_series(data, x, y, label=None, palette=None):
+def plot_time_series(data, x, y, label=None, palette=None, period=10):
     # Visualize annotation
     plt.figure(figsize=(30, 10))
     if label:
-        ax = sns.pointplot(x=x, y=y, data=data.loc[::10], hue=label, palette=palette)
+        ax = sns.pointplot(x=x, y=y, data=data.loc[::period], hue=label, palette=palette)
     else:
-        ax = sns.pointplot(x=x, y=y, data=data.loc[::10])
+        ax = sns.pointplot(x=x, y=y, data=data.loc[::period])
     for ind, label in enumerate(ax.get_xticklabels()):
-        if ind % 10 == 0:  # every 10th label is kept
+        if ind % period == 0:  # every 10th label is kept
             label.set_visible(True)
         else:
             label.set_visible(False)
